8f20b87d68587ce1684f8659ba9cdbd38025a7a3
NEXUS\cyurr <c.yurre@gmail.com>
2025-11-16 19:46:05 +0100

    rutina de link de paths sacada fuera.

---
+++ cy-geometry/cy-geometry-link-paths.js
@@ -0,0 +1,95 @@
+
+import { fuzzy_eq_point, blockReverse } from "./cy-geometry-library.js";
+import { createDrawElement } from "./cy-geometry-basic-elements.js";
+/**
+ * 
+ * @param tol @todo solucionar el paso de la base de datos...
+ * @returns 
+ */
+
+
+export function linkPaths(allPaths, tol = 0.1){
+    //Algoritmo 1, junta los que están seguidos y convierte todo a pthas (con su pi, pf, bbox)
+    function blocksToPaths( allBlocks, tol){
+        //rutina link de toda una capa
+        let paths = []; //Donde los vamos a dejar
+        //el primer bloque no empalma con nada y nos va a meter un array vacío en paths.
+        //Lo quitaremos al final, pero el bucle queda más limpio
+        let linked = [], head = {x: Infinity, y:Infinity}, tail = {x:Infinity, y:Infinity};
+         //array de referencias, para hacer shifts sin machacar el original
+        const blocks = allBlocks.filter(b => (b.type === 'segment' || b.type === 'arc' || b.type === 'bezier'));
+        let b;
+        for(let ix=0; ix < blocks.length; ix++){
+            b = blocks[ix];
+            if(fuzzy_eq_point(tail, b.pi, tol)){ //por la "derecha"
+                linked.push(b);
+                tail = b.pf;
+            } else if(fuzzy_eq_point(tail, b.pf, tol)){            //por la "derecha" invertido
+                linked.push( blockReverse(b));                      //Tiene que ser un reverse no destructivo porque si no no furrula
+                tail = b.pi;
+            } else if(fuzzy_eq_point(head, b.pf, tol)){ //por la "izquierda"
+                linked.unshift(b);
+                head = b.pi;
+            } else if(fuzzy_eq_point(head, b.pi, tol)){ //por la izquierda invirtiendo
+                linked.unshift(blockReverse(b));
+                head = b.pf;
+            } else { //Uno que no empalma...el primero será uno vacío
+                paths.push(linked); //solo guardo de momento los elementos
+                linked = [b];
+                }
+            }
+        paths.shift(); //el morrallero inicial
+        paths = paths.map(p => createDrawElement('path', {elements : p}));
+        //le pegamos los que ya eran paths en origen , si los hubiera
+        paths = paths.concat(blocks.filter( b => b.type === 'path'));
+        return paths;
+    }
+
+    //Algoritmo 2, los paths todos contra todos
+    //TODO meter los bboxes para que salga si no empalma
+    function stitch(paths, tol){
+        let linkedPaths = [];
+        let linked = [], head, tail;
+        let dummy = 0;
+        while(1){
+            console.log(dummy++);
+            let p = paths.find(p=>!p.visited); //La primera vez será el primero
+            if(p === undefined) break;         //No quedan slices sin visitar
+            linked = p.elements;
+            head = p.pi, tail = p.pf;
+            p.visited = true;
+            for(let ix=0; ix < paths.length; ix++){
+                const b= paths[ix]
+                if(b.visited) continue;
+                if(fuzzy_eq_point(tail, b.pi, tol)){ //por la "derecha"
+                    linked = linked.concat(b.elements);
+                    b.visited = true;
+                } else if(fuzzy_eq_point(tail, b.pf, tol)){            //por la "derecha" invertido
+                    linked = linked.concat( blockReverse(b));
+                    b.visited = true;
+                } else if(fuzzy_eq_point(head, b.pf, tol)){ //por la "izquierda"
+                    linked = b.elements.concat(linked);
+                    b.visited = true;
+                } else if(fuzzy_eq_point(head, b.pi, tol)){ //por la izquierda invirtiendo
+                    linked = blockReverse(b).elements.concat(linked);
+                    b.visited = true;
+                }
+                head = linked[0].pi; tail = linked[linked.length-1].pf;
+            }
+            linkedPaths.push(createDrawElement('path',{elements:linked})); //entra con visited a false
+        }
+        return linkedPaths;
+    }
+       //YURRE: Esto ye una conjetura, que la parte de intersección tiene menos trozos que las propias...
+    //La alterntiva es un bucle único con todos los slices juntos
+    let slices = blocksToPaths(allPaths, tol);
+    let nPaths = slices.length+1; //por inicializar y que pase. Cuando ya no mejora, se sale
+    while((nPaths > slices.length) && (slices.length > 1)  ){
+        nPaths = slices.length;
+        console.log(nPaths);
+        slices.forEach(s => s.visited = false);
+        slices = stitch(slices, tol);
+    }
+    //Aquí en slices quedan los trozos que no se dejan juntar
+    return slices;
+}
